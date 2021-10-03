215975f023944b98d3e25657bd5d173da0247141
AthilioCavalcanti <athiliocavalcanti@gmail.com>
2021-10-03 13:38:47 -0300

    Adicionando nova imagens e funções js

--- js/infoseries.js
+++ js/infoseries.js
@@ -1,6 +1,29 @@
-let whatIf = {
+// Séries animadas
+const whatIf = {
     'titulo': 'What if...?',
     'ano': 2021,
     'descricao': 'Baseada nos populares quadrinhos homônimos, a produção explorará histórias hipotéticas que poderiam ter mudado completamente o rumo do universo cinematográfico da editora.',
     'urlCapa': 'images/capa-what-if-2021.jpg'
+}
+
+const invencivel = {
+    'titulo': 'Invencível',
+    'ano': 2021,
+    'descricao': 'INVENCÍVEL é uma animação de super-heróis para adultos e conta a história de Mark Grayson, de 17 anos, um cara como qualquer outro de sua idade, exceto que seu pai é o super-herói mais poderoso do planeta, Omni-Man. Porém, à medida que Mark desenvolve seus próprios poderes, ele descobre que o legado de seu pai pode não ser tão heroico quanto parece.',
+    'urlCapa': 'images/capa-invencivel-2021.jpg'
+}
+
+// Séries live action
+const loki = {
+    'titulo': 'Loki',
+    'ano': 2021,
+    'descricao': 'Começando imediatamente depois que Loki rouba o Tesseract (de novo), ele se encontra diante da Autoridade de Variação Temporal, uma organização burocrática que existe fora do tempo e espaço. Forçado a responder por seus crimes contra a linha do tempo, ele recebe uma escolha: Ser deletado da realidade ou ajudar a capturar uma ameaça ainda maior.',
+    'urlCapa': 'images/capa-loki-2021.jpg'
+}
+
+const theFlash = {
+    'titulo': 'The Flash',
+    'ano': 2014,
+    'descricao': 'Um perito forense desperta de um coma com poderes especiais que serão postos à prova na luta contra forças que ameaçam a cidade.',
+    'urlCapa': 'images/capa-the-flash-2014.jpg'
 }
