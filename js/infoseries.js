c809cc5d7b8cc3f4ab19d5557af7831d112a366a
AthilioCavalcanti <athiliocavalcanti@gmail.com>
2021-10-05 10:35:44 -0300

    Inserindo mais pôsteres no carrossel de populares

--- js/infoseries.js
+++ js/infoseries.js
@@ -19,6 +19,16 @@ const invencivel = new Titulo(
     'images/poster-invencivel.jpg'
 )
 
+const marvelSpiderMan = new Titulo(
+    'Homem-Aranha da Marvel',
+    2017,
+    'Um adolescente inseguro mas corajoso e inteligente chamado Peter Parker, é picado por uma aranha radioativa e recebe poderes. Ele se torna um herói chamado Homem-Aranha.',
+    'Animação, Sci-Fi & Fantasy',
+    '21m',
+    'images/capa-homem-aranha-da-marvel-2017.jpg',
+    'images/poster-homem-aranha-da-marvel.jpg'
+)
+
 // Séries live action
 const loki = new Titulo(
     'Loki',
@@ -40,6 +50,16 @@ const theFlash = new Titulo(
     'images/poster-the-flash.jpg'
 )
 
+// Filmes animados
+const homemAranhaNoAranhaverso = new Titulo(
+    'Homem-Aranha: No Aranhaverso',
+    2018,
+    'Miles Morales é um jovem negro do Brooklyn que se tornou o Homem-Aranha inspirado no legado de Peter Parker, já falecido. Entretanto, ao visitar o túmulo de seu ídolo em uma noite chuvosa, ele é surpreendido com a presença do próprio Peter, vestindo o traje do herói aracnídeo sob um sobretudo. A surpresa fica ainda maior quando Miles descobre que ele veio de uma dimensão paralela, assim como outras variações do Homem-Aranha.',
+    'Ação, Aventura, Animação, Ficção científica, Comédia',
+    '1h57mim',
+    'images/capa-homem-aranha-no-aranhaverso-2018.jpg',
+    'images/poster-homem-aranha-no-aranhaverso.jpg'
+)
 
 // Filmes live action
 const venom = new Titulo(
@@ -50,4 +70,14 @@ const venom = new Titulo(
     '1h52min',
     'images/capa-venom-2018.jpg',
     'images/poster-venom.jpg'
+)
+
+const capitaMarvel = new Titulo(
+    'Capitã Marvel',
+    2019,
+    'Capitã Marvel, parte do exército de elite dos Kree, uma raça alienígena, encontra-se no meio de uma batalha entre seu povo e os Skrulls. Ao tentar impedir uma invasão de larga escala na Terra, em 1995, ela tem memórias recorrentes de uma outra vida, como Carol Danvers, agente da Força Aérea norte-americana. Com a ajuda de Nick Fury, Capitã Marvel precisa descobrir os segredos de seu passado e pôr um fim ao conflito intergalático com os Skrulls.',
+    'Ação, Aventura, Ficção científica',
+    '2h04min',
+    'images/capa-capita-marvel-2019.jpg',
+    'images/poster-capita-marvel.jpg'
 )
--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 let meuCSS = `background: linear-gradient(90deg, rgba(0, 0, 0, 0.92)50%, rgba(0, 0, 0, 0.001))`
 
-function trocaBanner(urlImage, titulo, ano, descricao){
+function trocaBanner(titulo){
     const movie = document.querySelector('div.banner-principal')
     const tituloMovie = document.querySelector('div.titulo')
     const descricaoMovie = document.querySelector('div.descricao')
 
-    movie.style.cssText = `${meuCSS}, url(${urlImage})`
-    tituloMovie.innerHTML = `<h2>${titulo} (${ano})</h2>`
-    descricaoMovie.innerHTML = `<p>${descricao}</p>`
+    movie.style.cssText = `${meuCSS}, url(${titulo.urlCapa})`
+    tituloMovie.innerHTML = `<h2>${titulo.nome} (${titulo.ano})</h2>`
+    descricaoMovie.innerHTML = `<p>${titulo.descricao}</p>`
 
     window.scrollTo(0, 0)
 }
