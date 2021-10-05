2ae7842f4f9ed1b69598fa5a615164b6005b85b7
AthilioCavalcanti <athiliocavalcanti@gmail.com>
2021-10-05 10:39:06 -0300

    Movendo função de trocar o banner principal para o arquivo main

--- js/titulo.js
+++ js/titulo.js
@@ -8,17 +8,4 @@ class Titulo{
         this.urlCapa = urlCapa
         this.urlPoster = urlPoster
     }
-
-    banner(){
-        let meuCSS = `background: linear-gradient(90deg, rgba(0, 0, 0, 0.92)50%, rgba(0, 0, 0, 0.001))`
-        const movie = document.querySelector('div.banner-principal')
-        const tituloMovie = document.querySelector('div.titulo')
-        const descricaoMovie = document.querySelector('div.descricao')
-    
-        movie.style.cssText = `${meuCSS}, url(${this.urlCapa})`
-        tituloMovie.innerHTML = `<h2>${this.nome} (${this.ano})</h2>`
-        descricaoMovie.innerHTML = `<p>${this.descricao}</p>`
-    
-        window.scrollTo(0, 0)
-    }
 }
